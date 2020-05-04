7edc3ea5fff2fb72572114c262f6322ed3c65077
ThanushaJ <thanusnani@gmail.com>
2020-05-04 12:16:11 +0400

    using ES6 features

--- assoc.js
+++ assoc.js
@@ -1,12 +1,14 @@
 // const result = assoc("c", 3, { a: 1, b: 2 });
 // console.log(result); //=> {a: 1, b: 2, c: 3}
 
-function assoc(x, y, z) {
-  let result = {};
+function assoc(key, value, obj) {
+  //   let result = {};
 
-  for (let key of Object.keys(z)) {
-    result[key] = z[key];
-  }
-  result[x] = y;
+  //   for (let key of Object.keys(z)) {
+  //     result[key] = z[key];
+  //   }
+
+  let { ...result } = obj;
+  result[key] = value;
   return result;
 }
