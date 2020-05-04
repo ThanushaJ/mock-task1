ec1d4c4e39425e9bcdd02fdabcd050b7e375b432
ThanushaJ <thanusnani@gmail.com>
2020-05-04 12:12:31 +0400

    using ES6 features

--- dissoc.js
+++ dissoc.js
@@ -1,14 +1,15 @@
 //dissoc("b", { a: 1, b: 2, c: 3 }); //=> {a: 1, c: 3}
 
 function dissoc(prop, obj) {
-  let result = {};
+  //   let result = {};
+  let { ...result } = obj;
   if (obj.hasOwnProperty(prop)) {
-    for (let key in obj) {
-      result[key] = obj[key];
-    }
+    // for (let key in obj) {
+    //   result[key] = obj[key];
+    // }
     delete result[prop];
     return result;
   } else {
-    return "property undefined";
+    return result;
   }
 }
